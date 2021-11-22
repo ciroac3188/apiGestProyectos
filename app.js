48b78733956aa22ad21c2264e70fe60dc089c218
TeamARV <andresriveravega@msn.com>
2021-11-22 16:25:00 -0500

    Agrego servidor apollo con endpoint tipo  GraphQL

--- app.js
+++ app.js
@@ -3,8 +3,32 @@
 const express = require("express")
 const app = express()
 const conexion = require("./db")
+////////////////imports sacados de documentacion npm
+var cors = require('cors')
+app.use(cors())
+const { ApolloServer, gql } = require('apollo-server-express');
+///////////////////////////
+// The GraphQL schema
+const typeDefs = gql`
+  type Query {
+    "A simple type for getting started!"
+    hello: String
+  }
+`;
 
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => 'world',
+  },
+};
 
+///////defino server apollo
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+//////////
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
@@ -20,9 +44,12 @@ app.use(avances)
 app.use(inscripciones)
 
 app.get("/",(req,res)=>{
-res.send(" <h1><a href=\'http://localhost:3000/usuarios\'> Hola este es el backend de Insert Name 🐱‍👤</a></h1>")
+res.send(" <h1><a href=\'http://localhost:3000/usuarios\'> Hola este es el backend Tipo REST de Insert Name 🐱‍👤</a> </br> <a href=\'http://localhost:3000/graphql\'> Hola este es el backend Tipo GraphQL de Insert Name 🐱‍👤</a> </h1>")
 })
 
-app.listen(3000 ,()=>{
-console.log("BackEnd Insert Name working in 🖥️ -> http://localhost:3000")
+app.listen(3000 , async ()=>{
+console.log("BackEnd Tipo REST Insert Name working in 🖥️ -> http://localhost:3000")
+console.log("BackEnd Tipo GraphQL Insert Name working in 🕸️ -> http://localhost:3000/graphql")
+await server.start()
+await server.applyMiddleware({app})
 })
